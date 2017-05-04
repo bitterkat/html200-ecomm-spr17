19a0b44fe96eb1aa6354505f01c95108e6701544
jlc <bitterkat@gmail.com>
2017-05-04 13:36:26 -0700

    adds form handler

---
+++ js/form-handler.js
@@ -0,0 +1,11 @@
+console.log("I am the handler of forms");
+
+function getEmail () {
+  event.preventDefault();
+  var email = document.customerEmail.email.value;
+  if (email != "") {
+    console.log("Thanks for signing up for our mailing list, " + email + "!!");
+  } else {
+    console.log("Please enter an email address!");
+  }
+}
