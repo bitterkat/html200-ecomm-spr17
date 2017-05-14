1dcec2331783b30638eadbc081616cd6ce42894a
[Jennifer Carrillo] <[bitterkat@gamil.com]>
2017-05-14 16:43:19 -0700

    cleans up js cart code

--- js/form-handler.js
+++ js/form-handler.js
@@ -11,27 +11,3 @@ function getEmail () {
     console.log("Please enter an email address!");
   }
 }
-
-function addToCart(id,name) {
-  event.preventDefault();
-  var singleProduct = {name: name, id: id};
-  cart.push({singleProduct});
-  console.log(singleProduct);
-  console.log(cart.length);
-  return cart;
-}
-
-function removeFromCart(id,name) {
-  event.preventDefault();
-//   var singleProduct = {name: name, id: id};
-//   if (cart.length == 0) {
-//     console.log("nothing in cart");
-//   }
-//   else if //look for productid and find it's index, then splice it
-//   console.log("removed " + singleProduct);
-// } else {
-//   console.log("product not in cart");
-}
-
-  // console.log(singleProduct);
-  // console.log(cart.length);
